363dd8d5a9ebb8fe84e7f8f3abfd0a05309beb68
Joel Cornfield <157437180+Joel-Cornfield@users.noreply.github.com>
2024-12-13 10:05:22 +0800

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPluging = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPluging({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|mp4|wav)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+              },
+        ],
+    },
+}
